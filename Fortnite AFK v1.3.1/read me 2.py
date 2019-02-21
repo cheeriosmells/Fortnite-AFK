bdd3455d33e7fc6324bc9a2d4b8e241665a9d73b
cheeriosmells <47843331+cheeriosmells@users.noreply.github.com>
2019-02-21 01:16:25 -0500

    Add files via upload

---
+++ Fortnite AFK v1.3.1/read me 2.py
@@ -0,0 +1,20 @@
+import time
+
+print ('''
+Fortnite AFK v1.3 is the first public access version of cheerio's Fortnite AFK and is still in work in progress.
+
+INFORMATION:
+1.) When pressing the "Quit" button it does not completely exit the program but it does stop the program from responding which effectively stops the program from moving your mouse and pressing keys.
+2.) The "Placeholder" button will eventually become the "Start" button but as of now the program starts moving your mouse and pressing keys as soon as you open the program.
+3.) Your monitor resolution must be set to 1920x1080 for the program to work.
+4.) This version of Fortnite AFK is only for Windows OS.
+
+DOWNLOAD REQUIREMENTS:
+Fortnite AFK requires PyAutoGUI
+
+How to install:
+Open CMD as administrator and type in the following, "pip.exe install pyautogui"
+
+When all steps are complete run the file Fortnite AFK v1.3.1.
+''')
+time.sleep(1000000)
